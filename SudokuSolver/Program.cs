d191623a4e4a904556b2b1f4f6da5a0cf9af1f58
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-05-06 18:37:59 -0500

    jagged array vs a matrix

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -6,7 +6,12 @@ namespace SudokuSolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[][] jagged = new int[10][];
+            jagged[0] = new int[90];
+            jagged[1] = new int[80];
+
+            int[,] matrix = new int[10, 10];
+
         }
     }
 }
